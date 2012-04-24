691f113088e484b7a237d09df4da8339f4f21cfe
Chris Jobling <c.p.jobling@swansea.ac.uk>
2012-04-24 10:33:58 +0100

    Made a rails app

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Eg259Songs::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eg259Songs::Application.config.secret_token = '9d2bbbcd3f6da1606cce75f7658da47918b64d632625c7b6902079ecd4bb28e2757965fe1645fdad814038f49ef96656236c49a388da868dd91ca57e28fa3354'
